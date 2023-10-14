a40c9cfd1caf087d0599dfe48bb1c3d6ca568185
phucduongminh <phucbopbnation@gmail.com>
2023-10-14 16:51:33 +0700

    Fix Lab1

--- Lab_1/HelloNameDialog.java
+++ Lab_1/HelloNameDialog.java
@@ -3,8 +3,8 @@ import javax.swing.JOptionPane;
 public class HelloNameDialog {
     public static void main(String[] args) {
         String name;
-        name = JOptionPane.showInputDialog("Please enter your name:");
-        JOptionPane.showMessageDialog(null, "Hi"+ name +"!");
+        name = JOptionPane.showInputDialog("Duong Minh Phuc - 4648 Please enter your name:");
+        JOptionPane.showMessageDialog(null, "Hi "+ name +"!");
         System.exit(0);
     }
 }
--- Lab_1/HelloWorld.java
+++ Lab_1/HelloWorld.java
@@ -4,6 +4,7 @@
 public class HelloWorld {
 
     public static void main(String[] args) {
+        System.out.println("Duong Minh Phuc - 4648");
         System.out.println("Xin chao \n cac ban.");
         System.out.println("Hell \t World");
     }
